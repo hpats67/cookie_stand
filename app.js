427f6133043f8df262cafdc174a8d8bff3f7136e
hpats67 <hpats67@gmail.com>
2016-08-28 00:25:00 -0700

    partial completion of tuesday assignment

--- app.js
+++ app.js
@@ -1,222 +1,58 @@
 'use strict';
 
-var pike = {
-  minCust: 23,
-  maxCust: 65,
-  avgCookies: 6.3,
-  totalCookies: 0,
-  time: ['6 am', '7 am', '8 am', '9 am', '10 am', '11 am', '12 pm', '1 pm', '2 pm', '3 pm', '4 pm', '5 pm', '6 pm', '7 pm', '8 pm'],
-  total: [],
-//this will generate a random number to be used later
-  generateRandom: function() {
-    return Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);
-  },
-//this is used to generate a number for each hour of the day
-  cookiesPerHour: function() {
-    return Math.floor(this.avgCookies * this.generateRandom());
-  },
-//rendering the function to the html page
-  render: function() {
-    var ul = document.createElement('ul');
-    var h2 = document.createElement('h2');
+var time = ['6 am', '7 am', '8 am', '9 am', '10 am', '11 am', '12 pm',
+            '1 pm', '2 pm', '3 pm', '4 pm', '5 pm', '6 pm', '7 pm', '8 pm'];
 
-    h2.textContent = '1st & Pike';
-    ul.appendChild(h2);
-//creating the number for each hour and putting it into an array
-    for (var k = 0; k < this.time.length; k++) {
-      this.total.push(this.cookiesPerHour());
-//associating the random number to each hour and placing it in an li element
-      for(var b = 0; b < this.total.length; b++){
-        var li = document.createElement('li');
-        li.textContent = this.time[k] + ': ' + this.total[b] + ' Cookies';
-      }
-      ul.appendChild(li);
-    };
-//summing the hour totals to a total number
-    for(var q = 0; q < this.total.length - 1; q++){
-      this.totalCookies += this.total[q];
-    }
-//adding the total to the end of the list
-    li.textContent = 'Total: ' + this.totalCookies + 'Cookies';
-
-    var main = document.getElementById('store_info');
-    main.appendChild(ul);
-  }
+function Store(name, min, max, age) {
+  this.name         = name;
+  this.minCust      = min;
+  this.maxCust      = max;
+  this.avgCookies   = age;
+  this.totalCookies = 0;
+  this.total        = [];
 };
 
-var seatac = {
-  minCust: 3,
-  maxCust: 24,
-  avgCookies: 1.2,
-  totalCookies: 0,
-  time: ['6 am', '7 am', '8 am', '9 am', '10 am', '11 am', '12 pm', '1 pm', '2 pm', '3 pm', '4 pm', '5 pm', '6 pm', '7 pm', '8 pm'],
-  total: [],
-//this will generate a random number to be used later
-  generateRandom: function() {
-    return Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);
-  },
+Store.prototype.generateRandom = function() {
+  return Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);
+};
 //this is used to generate a number for each hour of the day
-  cookiesPerHour: function() {
-    return Math.floor(this.avgCookies * this.generateRandom());
-  },
+Store.prototype.cookiesPerHour = function() {
+  return Math.floor(this.avgCookies * this.generateRandom());
+};
 //rendering the function to the html page
-  render: function() {
-    var ul = document.createElement('ul');
-    var h2 = document.createElement('h2');
+Store.prototype.render = function() {
+  var ul = document.createElement('ul');
+  var h2 = document.createElement('h2');
 
-    h2.textContent = 'SeaTac Airport';
-    ul.appendChild(h2);
+  h2.textContent = this.name;
+  ul.appendChild(h2);
 //creating the number for each hour and putting it into an array
-    for (var k = 0; k < this.time.length; k++) {
-      this.total.push(this.cookiesPerHour());
+  for (var k = 0; k < time.length; k++) {
+    this.total.push(this.cookiesPerHour());
 //associating the random number to each hour and placing it in an li element
-      for(var b = 0; b < this.total.length; b++){
-        var li = document.createElement('li');
-        li.textContent = this.time[k] + ': ' + this.total[b] + ' Cookies';
-      }
-      ul.appendChild(li);
+    for(var b = 0; b < this.total.length; b++){
+      var li = document.createElement('li');
+      li.textContent = time[k] + ': ' + this.total[b] + ' Cookies';
     };
+    ul.appendChild(li);
+  };
 //summing the hour totals to a total number
-    for(var q = 0; q < this.total.length - 1; q++){
-      this.totalCookies += this.total[q];
-    }
+  for(var q = 0; q < this.total.length - 1; q++){
+    this.totalCookies += this.total[q];
+  };
 //adding the total to the end of the list
-    li.textContent = 'Total: ' + this.totalCookies + 'Cookies';
+  li.textContent = 'Total: ' + this.totalCookies + 'Cookies';
 
-    main.appendChild(ul);
-  }
+  var main = document.getElementById('store_info');
+  main.appendChild(ul);
 };
 
-var seattle = {
-  minCust: 11,
-  maxCust: 38,
-  avgCookies: 3.7,
-  totalCookies: 0,
-  time: ['6 am', '7 am', '8 am', '9 am', '10 am', '11 am', '12 pm', '1 pm', '2 pm', '3 pm', '4 pm', '5 pm', '6 pm', '7 pm', '8 pm'],
-  total: [],
-//this will generate a random number to be used later
-  generateRandom: function() {
-    return Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);
-  },
-//this is used to generate a number for each hour of the day
-  cookiesPerHour: function() {
-    return Math.floor(this.avgCookies * this.generateRandom());
-  },
-//rendering the function to the html page
-  render: function() {
-    var ul = document.createElement('ul');
-    var h2 = document.createElement('h2');
+var pike    = new Store('1st and Pike', 23, 65, 6.3);
+var seatac  = new Store('SeaTac Airport', 3, 24, 1.2);
+var seattle = new Store('Seattle Center', 11, 38, 3.7);
+var capital = new Store('Capital Hill', 20, 38, 2.3);
+var alki    = new Store('Alki', 2, 16, 4.6);
 
-    h2.textContent = 'Seattle Airport';
-    ul.appendChild(h2);
-//creating the number for each hour and putting it into an array
-    for (var k = 0; k < this.time.length; k++) {
-      this.total.push(this.cookiesPerHour());
-//associating the random number to each hour and placing it in an li element
-      for(var b = 0; b < this.total.length; b++){
-        var li = document.createElement('li');
-        li.textContent = this.time[k] + ': ' + this.total[b] + ' Cookies';
-      }
-      ul.appendChild(li);
-    };
-//summing the hour totals to a total number
-    for(var q = 0; q < this.total.length - 1; q++){
-      this.totalCookies += this.total[q];
-    }
-//adding the total to the end of the list
-    li.textContent = 'Total: ' + this.totalCookies + 'Cookies';
-
-    main.appendChild(ul);
-  }
-};
-
-var capital = {
-  minCust: 20,
-  maxCust: 38,
-  avgCookies: 2.3,
-  totalCookies: 0,
-  time: ['6 am', '7 am', '8 am', '9 am', '10 am', '11 am', '12 pm', '1 pm', '2 pm', '3 pm', '4 pm', '5 pm', '6 pm', '7 pm', '8 pm'],
-  total: [],
-//this will generate a random number to be used later
-  generateRandom: function() {
-    return Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);
-  },
-//this is used to generate a number for each hour of the day
-  cookiesPerHour: function() {
-    return Math.floor(this.avgCookies * this.generateRandom());
-  },
-//rendering the function to the html page
-  render: function() {
-    var ul = document.createElement('ul');
-    var h2 = document.createElement('h2');
-
-    h2.textContent = 'Capital Hill';
-    ul.appendChild(h2);
-//creating the number for each hour and putting it into an array
-    for (var k = 0; k < this.time.length; k++) {
-      this.total.push(this.cookiesPerHour());
-//associating the random number to each hour and placing it in an li element
-      for(var b = 0; b < this.total.length; b++){
-        var li = document.createElement('li');
-        li.textContent = this.time[k] + ': ' + this.total[b] + ' Cookies';
-      }
-      ul.appendChild(li);
-    };
-//summing the hour totals to a total number
-    for(var q = 0; q < this.total.length - 1; q++){
-      this.totalCookies += this.total[q];
-    }
-//adding the total to the end of the list
-    li.textContent = 'Total: ' + this.totalCookies + 'Cookies';
-
-    main.appendChild(ul);
-  }
-};
-
-var alki = {
-  minCust: 2,
-  maxCust: 16,
-  avgCookies: 4.6,
-  totalCookies: 0,
-  time: ['6 am', '7 am', '8 am', '9 am', '10 am', '11 am', '12 pm', '1 pm', '2 pm', '3 pm', '4 pm', '5 pm', '6 pm', '7 pm', '8 pm'],
-  total: [],
-//this will generate a random number to be used later
-  generateRandom: function() {
-    return Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);
-  },
-//this is used to generate a number for each hour of the day
-  cookiesPerHour: function() {
-    return Math.floor(this.avgCookies * this.generateRandom());
-  },
-//rendering the function to the html page
-  render: function() {
-    var ul = document.createElement('ul');
-    var h2 = document.createElement('h2');
-
-    h2.textContent = 'Alki';
-    ul.appendChild(h2);
-//creating the number for each hour and putting it into an array
-    for (var k = 0; k < this.time.length; k++) {
-      this.total.push(this.cookiesPerHour());
-//associating the random number to each hour and placing it in an li element
-      for(var b = 0; b < this.total.length; b++){
-        var li = document.createElement('li');
-        li.textContent = this.time[k] + ': ' + this.total[b] + ' Cookies';
-      }
-      ul.appendChild(li);
-    };
-//summing the hour totals to a total number
-    for(var q = 0; q < this.total.length - 1; q++){
-      this.totalCookies += this.total[q];
-    }
-//adding the total to the end of the list
-    li.textContent = 'Total: ' + this.totalCookies + 'Cookies';
-
-    main.appendChild(ul);
-  }
-};
-
-var main = document.getElementById('store_info');
 
 pike.render();
 seatac.render();
