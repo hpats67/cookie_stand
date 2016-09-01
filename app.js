093426b6893ada9810a7948d44b5d2fba4f2aa0e
hpats67 <hpats67@gmail.com>
2016-09-01 09:23:27 -0700

    just updating

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 'use strict';
 
 var time = [' ', '6 am', '7 am', '8 am', '9 am', '10 am', '11 am', '12 pm', '1 pm',
-            '2 pm', '3 pm', '4 pm', '5 pm', '6 pm', '7 pm', '8 pm', 'Totals'];
+            '2 pm', '3 pm', '4 pm', '5 pm', '6 pm', '7 pm', '8 pm',];
 
 function Store(name, min, max, age) {
-  this.name         = name;
-  this.minCust      = min;
-  this.maxCust      = max;
-  this.avgCookies   = age;
-  this.totalCookies = 0;
-  this.total        = [];
+  this.name         = name; //eslint-disable-line
+  this.minCust      = min; //eslint-disable-line
+  this.maxCust      = max; //eslint-disable-line
+  this.avgCookies   = age; //eslint-disable-line
+  this.totalCookies = 0; //eslint-disable-line
+  this.total        = []; //eslint-disable-line
 };
 
 Store.prototype.generateRandom = function() {
@@ -21,42 +21,48 @@ Store.prototype.cookiesPerHour = function() {
 };
 //rendering the function to the html page
 Store.prototype.render = function() {
-  var ul = document.createElement('ul');
-  var h2 = document.createElement('h2');
+  var tr = document.createElement('tr');
+  var th = document.createElement('th');
 
-  h2.textContent = this.name;
-  ul.appendChild(h2);
+  th.textContent = this.name;
+  salesTbl.appendChild(tr);
 //creating the number for each hour and putting it into an array
+Store.prototype.hourTotal = function() {
   for (var k = 0; k < time.length; k++) {
     this.total.push(this.cookiesPerHour());
 //associating the random number to each hour and placing it in an li element
     for(var b = 0; b < this.total.length; b++){
-      var li = document.createElement('li');
+      var td = document.createElement('td');
       li.textContent = time[k] + ': ' + this.total[b] + ' Cookies';
     };
-    ul.appendChild(li);
+    tr.appendChild(td);
   };
 //summing the hour totals to a total number
   for(var q = 0; q < this.total.length - 1; q++){
     this.totalCookies += this.total[q];
   };
 //adding the total to the end of the list
-  li.textContent = 'Total: ' + this.totalCookies + 'Cookies';
+  td.textContent = 'Total: ' + this.totalCookies + 'Cookies';
 };
 
-var pike    = new Store('1st and Pike', 23, 65, 6.3);
-var seatac  = new Store('SeaTac Airport', 3, 24, 1.2);
-var seattle = new Store('Seattle Center', 11, 38, 3.7);
-var capital = new Store('Capital Hill', 20, 38, 2.3);
-var alki    = new Store('Alki', 2, 16, 4.6);
-
+var pike     = new Store('1st and Pike', 23, 65, 6.3);
+var seatac   = new Store('SeaTac Airport', 3, 24, 1.2);
+var seattle  = new Store('Seattle Center', 11, 38, 3.7);
+var capital  = new Store('Capital Hill', 20, 38, 2.3);
+var alki     = new Store('Alki', 2, 16, 4.6);
 var salesTbl = document.createElement('table');
 
-
+// this creates a table header for times
 for(var hours = 0; hours < time.length; hours++){
   var th = document.createElement('th');
   th.textContent = time[hours];
   salesTbl.appendChild(th);
+}
+
+  var tr = document.createElement('tr');
+
+for(var store = 0; store < time.length; store++){
+
 }
 
 var main = document.getElementById('store_info');
